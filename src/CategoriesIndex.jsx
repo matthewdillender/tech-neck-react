25590823c5a895308aca390b7cc808e9779d0d9e
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-05-05 22:19:03 -0700

    category cards as buttons set up

--- src/CategoriesIndex.jsx
+++ src/CategoriesIndex.jsx
@@ -1,14 +1,24 @@
+import "./CategoriesIndex.css";
+
 export function CategoriesIndex(props) {
   return (
-    <div>
-      <h1>All Categories</h1>
-      {props.categories.map((category) => (
-        <div key={category.id}>
-          <h2>{category.name}</h2>
-          <img src={category.category_image_url} />
-          <h3>Description: {category.description}</h3>
-        </div>
-      ))}
+    <div className="categories-container">
+      <h1>Exercise Categories</h1>
+      <div className="category-list">
+        {props.categories.map((category) => (
+          <button key={category.id} className="category-card">
+            <div>
+              <h2>{category.name}</h2>
+              <img
+                src={category.category_image_url}
+                alt={category.name}
+                style={{ maxWidth: "15%", height: "auto" }} // Inline styling for image size
+              />
+              <p>Description: {category.description}</p>
+            </div>
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
