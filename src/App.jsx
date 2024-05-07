f2bba1913919d7e8d19709d308cc757a946a7b6b
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-05-06 17:37:51 -0700

    default user id

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Footer } from "./Footer";
 import { LoginModal } from "./LoginModal";
 
 function App() {
-  const defaultUser = { id: 1, username: "Guest" }; // Default user information
+  const defaultUser = { id: 3, username: "Guest" }; // Default user information
   const [currentUser, setCurrentUser] = useState(defaultUser); // State to manage current user
 
   return (
